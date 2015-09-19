79bbc2276a36c8f3cf124eb8506492aef1e8d2c2
fehu <lord.fehu@gmail.com>
2015-09-19 01:13:12 -0500

    added SortedPossibilities SortedMap wrapper;

--- src/main/scala/feh/tec/astar/A_*.scala
+++ src/main/scala/feh/tec/astar/A_*.scala
@@ -1,10 +1,11 @@
 package feh.tec.astar
 
+import scala.collection.SortedMap
 import scala.collection.immutable.{HashSet, SortedSet}
 
-/** A* abstract algorithm.
- */
-trait AStar[T] {
+/** A* abstract algorithm. */
+trait A_*[T] {
+  import A_*._
 
   type Heuristic
   type Error
@@ -16,14 +17,14 @@ trait AStar[T] {
   /** Searches for a solution.
    *
    * @param initial The initial state.
-   * @return <code>Some(solution)</code> or <code>None</code> if the solution wasn't found.
+   * @return `Some(solution)` or `None` if the solution wasn't found.
    */
   def search(initial: T): Result
 
   /** Lists the next possible states.*/
   def transformations: T => Seq[T]
 
-  def heuristic: T => Heuristic
+  implicit def heuristic: T => Heuristic
 
   def isSolution: T => Boolean
 
@@ -32,15 +33,12 @@ trait AStar[T] {
 
   protected def error: String => Error
 
-  protected type ExtractedOpt = Option[(T, SortedSet[T])]
+  protected type ExtractedOpt = Option[(T, SortedPossibilities[Heuristic, T])]
 
-  protected def extractTheBest(open: SortedSet[T]): ExtractedOpt
+  protected def extractTheBest(open: SortedPossibilities[Heuristic, T]): ExtractedOpt
 
 
   // Implemented
-
-
-  implicit def tparamOrdering: Ordering[T] = Ordering.by(heuristic)
 
   protected type Decide = PartialFunction[ExtractedOpt, Result]
 
@@ -50,7 +48,7 @@ trait AStar[T] {
 
   protected def searchInner(state: T,
                             count: Long,
-                            open: SortedSet[T],
+                            open: SortedPossibilities[Heuristic, T],
                             closed: HashSet[T]): Result = {
     val newStates = transformations(state)
     val newOpen   = open ++ newStates
@@ -63,7 +61,7 @@ trait AStar[T] {
 
     def makeDecision = decisionLogic orElse searchInnerExtraLogic
 
-    def extract(from: SortedSet[T]): ExtractedOpt = extractTheBest(from) match{
+    def extract(from: SortedPossibilities[Heuristic, T]): ExtractedOpt = extractTheBest(from) match{
       case Some((best, opn)) if closed contains best => extract(opn)
       case other                                     => other
     }
@@ -72,3 +70,35 @@ trait AStar[T] {
   }
 
 }
+
+object A_*{
+
+  class SortedPossibilities[H, T](val underlying: SortedMap[H, List[T]])
+                                 (implicit ord: Ordering[H], heuristic: T => H)
+  {
+
+    def +(ts: T*) = ++(ts)
+
+    def ++(ts: Seq[T]) = new SortedPossibilities(insert(ts.toList, underlying))
+
+    def insert(ts: List[T], into: SortedMap[H, List[T]]): SortedMap[H, List[T]] = ts match {
+      case Nil       => into
+      case t :: tail =>
+        val h  = heuristic(t)
+        val l  = into.get(h) map (t :: _) getOrElse List(t)
+        val mp = into + (h -> l)
+        insert(tail, mp)
+    }
+
+    def head       = underlying.head
+    def headOption = underlying.headOption
+    def tail       = underlying.tail
+
+    def last       = underlying.last
+    def lastOption = underlying.lastOption
+    def init       = underlying.init
+
+  }
+
+}
+
