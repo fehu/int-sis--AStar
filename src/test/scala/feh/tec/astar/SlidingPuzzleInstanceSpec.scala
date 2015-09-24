526c050d98cfdc206ccecdae4b6ece7fcfc1c58c
fehu <lord.fehu@gmail.com>
2015-09-24 18:31:13 -0500

    implemented `test_equals` tests;

--- src/test/scala/feh/tec/astar/SlidingPuzzleInstanceSpec.scala
+++ src/test/scala/feh/tec/astar/SlidingPuzzleInstanceSpec.scala
@@ -2,7 +2,7 @@ package feh.tec.astar
 
 import feh.tec.astar.SlidingPuzzleInstanceSpec.InstanceData
 import feh.tec.puzzles.SlidingPuzzle.{Coordinate, Direction, GenericSlidingPuzzleInstance}
-import feh.tec.puzzles.{SlidingPuzzle, SlidingPuzzleInstance, SlidingPuzzleInt3x3v1}
+import feh.tec.puzzles.{SlidingPuzzleInt3x3v2, SlidingPuzzle, SlidingPuzzleInstance, SlidingPuzzleInt3x3v1}
 import feh.util._
 import org.scalacheck.{Arbitrary, Gen, Prop}
 import org.specs2._
@@ -21,6 +21,8 @@ trait SlidingPuzzleInstanceSpec[Impl[_] <: SlidingPuzzleInstance[_]] extends Spe
 
   def instanceFromData: InstanceData => Impl[_]
 
+  protected def someOtherPuzzleInstance: InstanceData => Impl[_]
+
   implicit def randomInstance: Arbitrary[Impl[_]] = Arbitrary{
     implicitly[Arbitrary[InstanceData]].arbitrary.map(instanceFromData)
   }
@@ -36,8 +38,8 @@ trait SlidingPuzzleInstanceSpec[Impl[_] <: SlidingPuzzleInstance[_]] extends Spe
    know the parent instance                 $test_parentInstance
    know its "generation"                    $test_generation
    equals another instance if and only if
-     1. has the same pieces configuration
-     2. is an instance of the same puzzle   $test_equals
+     1. has the same pieces configuration   $test_equals_conf
+     2. is an instance of the same puzzle   $test_equals_puzzle
 """
 
   def test_pieceAt = prop{
@@ -129,20 +131,51 @@ trait SlidingPuzzleInstanceSpec[Impl[_] <: SlidingPuzzleInstance[_]] extends Spe
       inst mustEqual instanceFromData(data)
   }
 
+  protected def getChildren(inst: Impl[_], empty: Coordinate) = Direction.all.toList
+    .flatMap(d => inst.tryMove(dirFrom(d, empty), d.opposite).toOption.asInstanceOf[Option[Impl[_]]])
+
+  protected def produceGenerations(from: Set[Impl[_]], c: Int): Set[Impl[_]] =
+    if (c == 0) from
+    else {
+      val next = from.flatMap(i => i.emptyPositions.flatMap(empty => getChildren(i, empty)))
+      produceGenerations(next, c-1)
+    }
+
   def test_parentInstance = prop{
     inst: Impl[_] =>
 
       val empty = inst.emptyPositions.head
-      val children = Direction.all.toList
-        .flatMap(d => inst.tryMove(dirFrom(d, empty), d.opposite).toOption.asInstanceOf[Option[Impl[_]]])
+      val children = getChildren(inst, empty)
 
       forall(children){
         _.parentInstance === Some(inst)
       }.iff(children.nonEmpty)
   }
 
-  def test_generation: Fragments     = todo
-  def test_equals: Fragments         = todo
+  def test_generation = prop{
+    inst: Impl[_] =>
+      val nGenerations = Gen.chooseNum(1, 10).sample.get
+      val generation = produceGenerations(Set(inst), nGenerations)
+      forall(generation){ _.generation === nGenerations }
+  }
+
+  def test_equals_conf =
+    prop{ //  must have the same pieces configuration
+      (inst1: Impl[_], inst2: Impl[_]) =>
+        if (inst1.listRows == inst2.listRows) inst1 mustEqual inst2
+        else                                  inst1 mustNotEqual inst2
+    }
+
+  def test_equals_puzzle =
+    prop{ // must reference the same puzzle
+      data: InstanceData =>
+        val thisInst = instanceFromData(data)
+        val thatInst = someOtherPuzzleInstance(data)
+
+        assert(thisInst.puzzle != thatInst.puzzle)
+
+        (thisInst.listRows mustEqual thatInst.listRows) and (thisInst mustNotEqual thatInst)
+    }
 }
 
 object SlidingPuzzleInstanceSpec{
@@ -151,7 +184,8 @@ object SlidingPuzzleInstanceSpec{
 
 abstract class GenericSlidingPuzzleInstanceSpec[Piece](val puzzle: SlidingPuzzle[Piece],
                                                        val randomInstanceData: Arbitrary[InstanceData],
-                                                       val instanceFromData: InstanceData => GenericSlidingPuzzleInstance[Piece] )
+                                                       val instanceFromData: InstanceData => GenericSlidingPuzzleInstance[Piece],
+                                                       val someOtherPuzzleInstance: InstanceData => GenericSlidingPuzzleInstance[Piece])
   extends SlidingPuzzleInstanceSpec[GenericSlidingPuzzleInstance]
 {
   def implementationName = "GenericSlidingPuzzleInstance"
@@ -160,12 +194,14 @@ abstract class GenericSlidingPuzzleInstanceSpec[Piece](val puzzle: SlidingPuzzle
 class GenericSlidingPuzzleInstanceSpecExample1 extends GenericSlidingPuzzleInstanceSpec[Int](
   GenericSlidingPuzzleInstanceSpecExample1.puzzle,
   GenericSlidingPuzzleInstanceSpecExample1.randomData,
-  GenericSlidingPuzzleInstanceSpecExample1.mkInstance
+  GenericSlidingPuzzleInstanceSpecExample1.mkInstance,
+  GenericSlidingPuzzleInstanceSpecExample1.someOtherInstance
 )
 
 
 object GenericSlidingPuzzleInstanceSpecExample1{
   lazy val puzzle = new SlidingPuzzleInt3x3v1
+  protected lazy val anotherPuzzle = new SlidingPuzzleInt3x3v2
 
   def randomData: Arbitrary[InstanceData] = Arbitrary{
     val pieces = None +: (1 to 8).map(Option(_)) |> (_.randomOrder)
@@ -181,10 +217,15 @@ object GenericSlidingPuzzleInstanceSpecExample1{
     }
   }
 
-  def mkInstance: InstanceData => GenericSlidingPuzzleInstance[Int] = {
-    data =>
-      val rows = data.groupBy(_._1._2).mapValues(_.toList.sortBy(_._1._1).map(_._2)).toList.sortBy(_._1).map(_._2)
-      new GenericSlidingPuzzleInstance[Int](puzzle, rows.asInstanceOf[Seq[Seq[Option[Int]]]], None, 0)
+  protected def newInstance: SlidingPuzzle[Int] => InstanceData => GenericSlidingPuzzleInstance[Int] = {
+    pzl =>
+      data =>
+        val rows = data.groupBy(_._1._2).mapValues(_.toList.sortBy(_._1._1).map(_._2)).toList.sortBy(_._1).map(_._2)
+        new GenericSlidingPuzzleInstance[Int](pzl, rows.asInstanceOf[Seq[Seq[Option[Int]]]], None, 0)
   }
+
+
+  def mkInstance = newInstance(puzzle)
+  def someOtherInstance = newInstance(anotherPuzzle)
 
 }
