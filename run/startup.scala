9f43a90410590ac7db712e74c016b6500abd79a7
fehu <lord.fehu@gmail.com>
2015-10-01 20:36:05 -0500

    a portable application with all the dependencies and startup script; todo: windows 'bat' file;

---
+++ run/startup.scala
@@ -0,0 +1,10 @@
+
+import feh.tec.puzzles.solve.run._
+import SlidingPuzzleExamples._
+
+
+println("Usage: ")
+println(" EXAMPLE.withSolver(H._??).run()")
+println(" where EXAMPLEs are Example1, Example2, Example3;")
+println("       Heuristics are H._01, H._02, H._03 ")
+println(" see feh/tec/puzzles/solve/run/SlidingPuzzles.scala")
