d9b1aee1cad36b90b67d6cfbce7881182da650bc
fehu <lord.fehu@gmail.com>
2015-10-02 01:32:59 -0500

    updated the jar;

--- run/run-console.sh
+++ run/run-console.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scala/bin/scala -classpath a-_2.11-0.4-SNAPSHOT.jar:util_2.11-1.0.9-SNAPSHOT.jar -i startup.scala
+scala/bin/scala -classpath A-Star_2.11-0.4-SNAPSHOT.jar:util_2.11-1.0.9-SNAPSHOT.jar -i startup.scala
